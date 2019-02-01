d89aaec0b497c743dbbd43269c3b5cb7ebfb6615
wonez <ffazlic@yahoo.com>
2019-02-01 02:11:07 +0100

    projects and media queries

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 const routes = require('./routes')
-
 const app = express();
 
 app.use('/', express.static(path.join(__dirname, 'public')))
--- routes.js
+++ routes.js
@@ -1,18 +1,54 @@
 const router = require('express').Router();
+const nodemailer = require('nodemailer');
 
 router.get('/', (req, res)=>{
     res.redirect('/projects')
 })
 
 router.get('/projects', (req, res)=>{
-    res.render('projects');
+    res.render('projects', {projects: true});
 })
 
 router.get('/about', (req, res)=>{
-    res.render('about');
+    res.render('about', {about: true});
 })
 
 router.get('/contact', (req, res)=>{
-    res.render('contact');
+    res.render('contact', {contact: true});
 })
+
+router.post('/form', (req, res)=>{
+    
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        secure: false,
+        port: 25,
+        secure: false,
+        auth: {
+            user: 'ffazlicme@gmail.com',
+            pass: 'Ffazlic123me'
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
+
+    let mailOptions = {
+        from: '"ffazlic.me " <ffazlicme@gmail.com>',
+        to: "ffazlic@yahoo.com", 
+        subject: "New Message On Portfolio Site",
+        text: `Name: ${req.body.name}\nEmail: ${req.body.email}\nMessage: ${req.body.message}\n`
+    };
+
+    transporter.sendMail(mailOptions, (err, info) => {
+        if(!err){
+            res.render('contact', { redirect: true, contact: true })    
+        }
+    })
+})
+
+router.get('*', (req, res) => {
+    res.redirect('/');
+})
+
 module.exports = router;
